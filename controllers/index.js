881ff7fa689d892e002d4bb740d30ceaacc6e860
marsuraa <tony2parker0@gmail.com>
2021-01-15 18:15:46 +0100

    Update index.js

--- controllers/index.js
+++ controllers/index.js
@@ -1,221 +1,221 @@
 const request = require("request-promise");
 const models = require("../schemas");
 let fs = require('fs');
-var host = "http://192.168.1.15:80";
-headers= {
+var host = "http://192.168.0.14:80";
+headers = {
     "Content-Type": "application/json",
-  }
-
-
-
-async function getPresenceSensor(req,res){
-    var url = host+"/getPresenceSensor"
-    var result = await request({
-        'url': url, 
-        'headers': headers,
-    });
-    res.json(result)
 }
 
-async function getIntensitySensor(req,res){
-    var url = host+"/getIntensitySensor"
+
+
+async function getPresenceSensor(req, res) {
+    var url = host + "/getPresenceSensor"
     var result = await request({
-        'url': url, 
+        'url': url,
         'headers': headers,
     });
-    res.json(result)
+    res.json(JSON.parse(result))
 }
 
-async function getPushSensor(req,res){
-    var url = ""
+async function getIntensitySensor(req, res) {
+    var url = host + "/getIntensitySensor"
     var result = await request({
-        'url': url, 
+        'url': url,
         'headers': headers,
     });
-    res.json(result)
+    res.json(JSON.parse(result))
 }
 
-async function getTouchSensor(req,res){
-    var url = ""
+
+
+async function getLedValues(req, res) {
+    var url = host + "/getLedValues"
     var result = await request({
-        'url': url, 
+        'url': url,
         'headers': headers,
     });
-    res.json(result)
+    res.json(JSON.parse(result))
 }
 
-async function getLedValues(req,res){
-    var url = host+"/getLedValues"
-    var result = await request({
-        'url': url, 
-        'headers': headers,
-    });
-    res.json(result)
-}
-
-async function getHpIsPlaying(req,res){
+async function getHpIsPlaying(req, res) {
 
     console.log(req.body.source)
     //saveBDD( req.body.id,{date: new Date(), source:source, variables: [{name: "state", value: String(state)},{name: "volume", value: String(volume)}]})
 }
 
-async function getTimeBeforeReplay(req,res){
-    var url = host+"/getTimeBeforeReplay"
+async function getTimeBeforeReplay(req, res) {
+    var url = host + "/getTimeBeforeReplay"
     var result = await request({
-        'url': url, 
+        'url': url,
         'headers': headers,
     });
-    res.json(result)
+    res.json(JSON.parse(result))
 }
 
-async function modifyColor(req,res){
-    var url = host+"/modifyColor"
+async function modifyColor(req, res) {
+    var url = host + "/modifyColor"
+    console.log(typeof req.body.intensity)
+    console.log(typeof req.body.R)
+    console.log(typeof req.body.G)
+    console.log(typeof req.body.B)
+    console.log(typeof req.body.mod)
     var state = req.body.state
     var intensity = req.body.intensity
-    var r = req.body.r
-    var g = req.body.g
-    var b = req.body.b
+    var r = req.body.R
+    var g = req.body.G
+    var b = req.body.B
     var source = req.body.source
-    var mod = req.body.led_mod
+    var mod = req.body.mod
     var result = await request.post({
-        'url': url, 
+        'url': url,
         'headers': headers,
-        formData : {
-            intensity:intensity,
-            R:r,
-            G:g,
-            B:b,
-            led_mod:mod
+        formData: {
+            intensity: intensity,
+            R: r,
+            G: g,
+            B: b,
+            led_mod: mod
         }
-        
+
     });
-    saveBDD(req.body.id,{date: new Date(), source:source, variables:[{name: "state", value: String(state)},{name: "intensity", value: String(intensity)},{name: "r", value: String(r),},{name: "g", value: String(g),},{name: "b", value: String(b),},{name: "mod", value: String(mod),}]})
-    res.json(result)
+    saveBDD(req.body.id, { date: new Date(), source: source, variables: [{ name: "state", value: String(state) }, { name: "intensity", value: String(intensity) }, { name: "r", value: String(r), }, { name: "g", value: String(g), }, { name: "b", value: String(b), }, { name: "mod", value: String(mod), }] })
+    //console.log(result)
+    res.json(JSON.parse(result))
 }
 
-async function modifyTimeBeforeReplay(req,res){
-    var url = host+"/modifyTimeBeforeReplay"
+async function modifyTimeBeforeReplay(req, res) {
+    var url = host + "/modifyTimeBeforeReplay"
     var time = req.body.time
     var result = await request.post({
-        'url': url, 
+        'url': url,
         'headers': headers,
-        formData : {
-            time:time,
+        formData: {
+            time: time,
         }
-        
+
     });
     res.json(result)
 }
 
 
-async function playHP(req,res){
-    var url = host+"/playHP"
+async function playHP(req, res) {
+    var url = host + "/playHP"
     var state = req.body.state
     var source = "API"
     var result = await request({
-        'url': url, 
+        'url': url,
         'headers': headers,
     });
-    saveBDD( req.body.id,{date: new Date(), source:source, variables: [{name: "state", value: String(state)}]})
+    saveBDD(req.body.id, { date: new Date(), source: source, variables: [{ name: "state", value: String(state) }] })
     res.json(result)
 }
 
 
-async function saveBDD(id,newValue){
-    try{
+async function saveBDD(id, newValue) {
+    try {
         await models.Device.updateOne(
-        { _id: id }, 
-        { $push: { stateList: [newValue] }});
-      }catch(e){
+            { _id: id },
+            { $push: { stateList: [newValue] } });
+    } catch (e) {
         console.error(e)
     }
 }
 
-async function getAllDevice(req,res){
+async function getAllDevice(req, res) {
     var devices;
-    try{
+    try {
         devices = await models.Device.find()
-    }catch(e){
+    } catch (e) {
         console.error(e);
         res.status(500).send("error requesting database");
 
-    }finally{
+    } finally {
         res.json(devices)
     }
 }
 
-async function getOneDevice(req,res){
+async function getOneDevice(req, res) {
     var deviceHist;
-    try{
+    try {
         deviceHist = await models.Device.findOne({ _id: req.params.id })
-    }catch(e){
+    } catch (e) {
         console.error(e);
         res.status(500).send("error requesting database");
 
-    }finally{
+    } finally {
         res.json(deviceHist)
     }
 }
 
-async function findUserByCredentials(req,res){
+async function findUserByCredentials(req, res) {
     var connexion;
-    try{
-        await models.User.findOne({ email: req.body.email, password: req.body.password}, function(err, result) {
-            if (err) { console.log(err);}
-                
+    try {
+        await models.User.findOne({ email: req.body.email, password: req.body.password }, function (err, result) {
+            if (err) { console.log(err); }
+
             if (result) {
                 connexion = "Found";
             } else {
                 connexion = "Not Found";
             }
         });
-    }catch(e){
+    } catch (e) {
         console.error(e);
         res.status(500).send("error requesting database");
 
-    }finally{
+    } finally {
         res.json(connexion)
     }
 
 }
 
-async function createUser(req,res){
+async function createUser(req, res) {
     var response = 0;
-    try{
-        console.log(req.body.nom);
-        const user = new models.User({
-            nom :  req.body.nom,
-            prenom:  req.body.prenom,
-            age :  req.body.age,
-            sexe : req.body.sexe,
-            email: req.body.email, 
-            password: req.body.password,
-            adresse :  req.body.adresse,
-            ville :  req.body.ville,
-            pays :  req.body.pays,
-        });
+    response = await models.User.exists({ email: req.body.email }, function (err, doc) {
+        if (err) {
+            console.log(err)
+        } else {
+            console.log("Result :", doc) // false 
+            response = doc;
+            if (!response) {
+                try {
+                    console.log(req.body.nom);
+                    const user = new models.User({
+                        nom: req.body.nom,
+                        prenom: req.body.prenom,
+                        age: req.body.age,
+                        sexe: req.body.sexe,
+                        email: req.body.email,
+                        password: req.body.password,
+                        adresse: req.body.adresse,
+                        ville: req.body.ville,
+                        pays: req.body.pays,
+                    });
+                    user.save();
+                } catch (e) {
+                    console.error(e);
+                    res.status(500).send("error requesting database");
 
-        user.save();
+                } finally {
+                    res.json("true")
+                }
+            }
+            else {
+                res.json("false")
+            }
+        }
+    });
 
-    }catch(e){
-        console.error(e);
-        res.status(500).send("error requesting database");
-
-    }finally{
-        res.json(response)
-    }
 }
 
-module.exports.modifyColor = modifyColor;  
-module.exports.getHpIsPlaying = getHpIsPlaying;  
-module.exports.getLedValues = getLedValues;  
-module.exports.getTouchSensor = getTouchSensor;  
-module.exports.getPushSensor = getPushSensor;  
-module.exports.getIntensitySensor = getIntensitySensor;  
-module.exports.getPresenceSensor = getPresenceSensor;  
+module.exports.modifyColor = modifyColor;
+module.exports.getHpIsPlaying = getHpIsPlaying;
+module.exports.getLedValues = getLedValues;
+module.exports.getIntensitySensor = getIntensitySensor;
+module.exports.getPresenceSensor = getPresenceSensor;
 module.exports.playHP = playHP;
 module.exports.getAllDevice = getAllDevice;
-module.exports.getOneDevice = getOneDevice; 
+module.exports.getOneDevice = getOneDevice;
 module.exports.modifyTimeBeforeReplay = modifyTimeBeforeReplay;
 module.exports.getTimeBeforeReplay = getTimeBeforeReplay;
 module.exports.findUserByCredentials = findUserByCredentials;
